56a0a7217153073bf5c5ea68a1b43dcafb89450a
keythnovar-cpu <keythnovar@gmail.com>
2025-08-18 00:17:20 +0530

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -149,7 +149,7 @@
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
     git
-    chrome
+    google-chrome
     firefox
     gnome-tweaks
     gnomeExtensions.dash-to-panel
